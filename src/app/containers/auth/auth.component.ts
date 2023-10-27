55fe62ad3d8fbdf4ba8c32277f3c80eb6e088581
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-10-27 23:17:39 +0200

    [core]: route package addition

--- src/app/containers/auth/auth.component.ts
+++ src/app/containers/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected route: Router) {this.route }
 
   ngOnInit(): void {
   }
