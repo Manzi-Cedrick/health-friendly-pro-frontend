fb35b26fd031868a6abc67a1f02c0a1a49498257
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-11-06 22:16:45 +0200

    [core]: unnecessary file refactoring handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TeamComponent } from './team/team.component';
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
 import { AuthComponent } from './containers/auth/auth.component';
 import { PatientComponent } from './containers/patient/patient.component';
