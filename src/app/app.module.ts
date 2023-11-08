6d342194678adde7fc0fab849ce2fda1983e7d44
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-11-08 23:40:53 +0200

    [perf]: new module referencing & connection  --addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TeamComponent } from './team/team.component';
 import { AuthComponent } from './containers/auth/auth.component';
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SideboxComponent } from './shared/sidebox/sidebox.component';
-import { PatientComponent } from './containers/patient/patient.component';
+import { TablePatientComponent } from './shared/table-patient/table-patient.component';
+import { InfoDrawerComponent } from './shared/info-drawer/info-drawer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeamComponent,
     AuthComponent,
     DashboardComponent,
     SidebarComponent,
     HeaderComponent,
     SideboxComponent,
-    PatientComponent
+    TablePatientComponent,
+    InfoDrawerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
