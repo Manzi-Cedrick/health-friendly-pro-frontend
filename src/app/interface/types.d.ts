d37bf33f60e187ed5dd434b77fd794b526196e4b
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-11-08 23:38:59 +0200

    [perf]: interface datatype --addition

---
+++ src/app/interface/types.d.ts
@@ -0,0 +1,13 @@
+export interface memberType {
+    string: string;
+    number: number;
+}
+export interface IUser {
+    id?:string;
+    username: string;
+    temperature: number[];
+    heartRate: number[];
+    date:string;
+    email:string;
+    frequentDisease:string;
+}
